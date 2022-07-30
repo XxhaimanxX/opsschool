d9594a0002e4b1419677dc363fd335dc2cd78839
israel <israelhaiman@gmail.com>
2022-07-30 17:23:18 +0300

    exercise5-fix_server1.sh/exercise5-fix_server2.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -2,3 +2,5 @@
 ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -C "vagrant@server1" -q -P ""
 cp /home/vagrant/.ssh/id_rsa.pub /vagrant
 sudo chmod 644 /home/vagrant/.ssh/id_rsa
+chmod +x /vagrant/fixScripts/exercise6-fix.sh
+cp /vagrant/fixScripts/exercise6-fix.sh /home/vagrant/
