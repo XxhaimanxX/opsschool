ebe0f72c7574cb645aa519e747776e125e15f50c
israel <israelhaiman@gmail.com>
2022-07-30 13:57:07 +0300

    exercise6-fix.sh

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+if [ $HOSTNAME == server1 ]
+then
+        for i in ${@:1:$#-1}
+        do
+                scp $i server2:${@: -1}
+                sum=$((sum + $(ls -l $i | awk '{print $5}')))
+        done
+else
+        for i in ${@:1:$#-1}
+        do
+                scp $i server1:${@: -1}
+                sum=$((sum + $(ls -l $i | awk '{print $5}')))
+        done
+fi
+echo $sum
+
